e7627e15b30b7b01b21c58714fea1a33bc40a7b6
zzb66666666x <18767066589@163.com>
2024-03-08 17:46:57 +0000

    s3 change

---
+++ basics/image_tr/write_para_s3.py
@@ -0,0 +1,29 @@
+import sys
+def change_bandwidth(file_path, new_value):
+    # Read the contents of the file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Find the line containing HPC_MAX_BANDWITH=1000 and replace it
+    for i, line in enumerate(lines):
+        
+        if 'MAX_BANDWIDTH_HPC=' in line:
+            lines[i] = f'MAX_BANDWIDTH_HPC={new_value}\n'
+            break
+
+    # Write the modified content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+
+# Example usage:
+if len(sys.argv) > 1:
+    new_bandwidth_value =sys.argv[1] 
+else:
+    print(f"Unknown value. Exiting")
+    sys.exit(1)
+file_path = '/home/ec2-user/share/Ray-Workloads/basics/image_tr/image_s3.py'
+file_path_sch='/home/ec2-user/ray/python/ray/scheduler/scheduler_constant.py'
+# Change this to the desired value
+change_bandwidth(file_path, new_bandwidth_value)
+
+change_bandwidth(file_path_sch,new_bandwidth_value)
